ac223e33cc8404f3ac270da545b01a79ed329198
bernish12 <bernish8888@gmail.com>
2026-09-24 20:56:16 +0530

    Add frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['titan-ai-frontend.onrender.com']
+  },
+  preview: {
+    allowedHosts: ['titan-ai-frontend.onrender.com']
+  }
 })
